5a7e2587f95b4cca774d8d928668ebc4ddd8d341
arifin <arifinrisyad848@gmail.com>
2024-05-28 09:11:51 +0700

    edit tampilan Dashboard-Admin

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers\Back;
 
-use App\Http\Controllers\Controller;
+use App\Models\Article;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Category;
 
 class DashboardController extends Controller
 {
     //
     public function index()
     {
-        return view('back.dashboard.index');
+        return view('back.dashboard.index', [
+            'total_article'    => Article::count(),
+            'total_categories' => Category::count(),
+            'latest_article'   => Article::with('category')->whereStatus('1')->latest()->take(5)->get(),
+            'popular_article'  => Article::with('category')->whereStatus('1')->orderBy('views', 'desc')->take(5)->get(),
+        ]);
     }
 }

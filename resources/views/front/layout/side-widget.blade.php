bbb0bd0d37f578715034fc85c59de01d60e9567e
arifin <arifinrisyad848@gmail.com>
2024-05-30 11:33:24 +0700

    Menampilkan Article terpopuler

--- resources/views/front/layout/side-widget.blade.php
+++ resources/views/front/layout/side-widget.blade.php
@@ -31,11 +31,26 @@
         <div class="card-body">You can put anything you want inside of these side widgets. They are easy to
             use, and feature the Bootstrap 5 card component!</div>
     </div>
-     <!-- Related Post-->
+     <!-- Popular Post-->
     <div class="card mb-4 shadow-sm">
-        <div class="card-header">Related Post</div>
+        <div class="card-header">Popular Post</div>
         <div class="card-body">
-
+         @foreach ($popular_posts as $item)
+         <div class="card mb-2">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="{{ asset('storage/back/'.$item->img)}}" alt="{{$item->title}}" class="img-fluid" >
+                </div>
+                <div class="col-md-9">
+                 <div class="card-body">
+                    <p class="card-title">
+                        <a href="{{url('p/'.$item->slug)}}">{{ $item->title}}</a>
+                       </p>
+                 </div>
+                </div>
+            </div>
+         </div>
+         @endforeach
         </div>
     </div>
 </div>
